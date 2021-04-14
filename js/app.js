893b5bfc76b5e9fe385b5a2aa60f108629817cb6
Amara002 <amaraalbalkhi@yahoo.com>
2021-04-14 16:46:09 +0300

    modify readme

--- js/app.js
+++ js/app.js
@@ -15,11 +15,11 @@ function Product(name, source) {
     this.shown = 0;
     Product.allProducts.push(this);
     namesArr.push(this.name);
-
+    // updateStorage();
 }
 
 Product.allProducts = [];
-Product.firstShown =  [];
+Product.firstShown = [];
 
 
 new Product('bag', 'img/bag.jpg');
@@ -43,7 +43,8 @@ new Product('usb', 'img/usb.gif');
 new Product('water-can', 'img/water-can.jpg');
 new Product('wine-glass', 'img/wine-glass.jpg');
 
-console.log(Product.allProducts);
+console.log( 'bro', Product.allProducts);
+console.log('scd', Product.firstShown);
 
 let maxAttempts = 10;
 let counter = 0;
@@ -66,7 +67,7 @@ function renderThreeImages() {
         || Product.firstShown.includes(leftElementIndex)
         || Product.firstShown.includes(middleElementIndex)
         || Product.firstShown.includes(rightElementIndex)) {
-        leftElementIndex = produceRandomIndex();    
+        leftElementIndex = produceRandomIndex();
         rightElementIndex = produceRandomIndex();
         middleElementIndex = produceRandomIndex();
 
@@ -84,11 +85,57 @@ function renderThreeImages() {
     rightElement.src = Product.allProducts[rightElementIndex].source;
     Product.allProducts[rightElementIndex].shown++;
 
-   
+
 
 }
 
 renderThreeImages();
+
+
+function updateStorage() {
+    console.log(JSON);
+    // let arrayString = JSON.stringify(votesArr);
+    // let arrayString1 = JSON.stringify(shownArr);
+    
+
+    // console.log(arrayString);
+    // console.log(arrayString1);
+
+    // localStorage.setItem('productVotes', arrayString);
+    // localStorage.setItem('productShown', arrayString1);
+
+
+    let arrayString = JSON.stringify(Product.allProducts);
+    localStorage.setItem('product', arrayString);
+
+}
+
+function getProduct() {
+
+    let data = localStorage.getItem('product');
+    let storeData = JSON.parse(data);
+    if (data !== null) {
+        Product.allProducts = storeData
+
+    }
+
+
+    // let votedData=JSON.parse(localStorage.getItem('productVotes'));
+    // let shownData=JSON.parse(localStorage.getItem('productShown'));
+
+    // // console.log(productData);
+
+    // // if the first time we visit the page, there will not be an array of objects inside the local storage so we should handle it here:
+    // if (votedData !==null && shownData !==null) {
+    //   votesArr=votedData;
+    //   shownArr=shownData;
+    // }
+
+    // // renderThreeImages();
+
+}
+
+
 
 votedProduct.addEventListener('click', handleUserClick);
 
@@ -127,6 +174,8 @@ function handleUserClick(event) {
 
         // show the chart
         chart();
+        updateStorage();
+
 
         votedProduct.removeEventListener('click', handleUserClick)
 
@@ -149,6 +198,7 @@ function handleUserClick(event) {
 
 
 }
+
 
 function chart() {
     let ctx = document.getElementById('myChart').getContext('2d');
@@ -189,3 +239,7 @@ function chart() {
     });
 
 }
+
+
+getProduct();
+
