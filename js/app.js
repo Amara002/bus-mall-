948e28194cacd53d1e4e88bfa65c1006c943b745
Amara002 <amaraalbalkhi@yahoo.com>
2021-04-14 05:15:32 +0300

    create a new branch

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 'use strict';
 
+let votedProduct = document.getElementById('voted product')
 let leftElement = document.getElementById('left');
 let middleElement = document.getElementById('middle');
 let rightElement = document.getElementById('right');
-let votedProduct = document.getElementById('voted product')
 
-
-function Product(name, source, times) {
+let namesArr = [];
+let votesArr = [];
+let shownArr = [];
+function Product(name, source) {
     this.name = name;
     this.source = source;
-    this.times = times;
     this.votes = 0;
     this.shown = 0;
     Product.allProducts.push(this);
+    namesArr.push(this.name);
 
 }
 
 Product.allProducts = [];
+Product.firstShown =  [];
+
 
 new Product('bag', 'img/bag.jpg');
 new Product('banana', 'img/banana.jpg');
@@ -41,7 +45,7 @@ new Product('wine-glass', 'img/wine-glass.jpg');
 
 console.log(Product.allProducts);
 
-let maxAttempts = 5;
+let maxAttempts = 10;
 let counter = 0;
 // let counter1 = 0;
 
@@ -58,60 +62,130 @@ function renderThreeImages() {
     middleElementIndex = produceRandomIndex();
     rightElementIndex = produceRandomIndex();
 
-    while (leftElementIndex === rightElementIndex || leftElementIndex === middleElementIndex || middleElementIndex === rightElementIndex) {
+    while (leftElementIndex === rightElementIndex || leftElementIndex === middleElementIndex || middleElementIndex === rightElementIndex
+        || Product.firstShown.includes(leftElementIndex)
+        || Product.firstShown.includes(middleElementIndex)
+        || Product.firstShown.includes(rightElementIndex)) {
+        leftElementIndex = produceRandomIndex();    
         rightElementIndex = produceRandomIndex();
         middleElementIndex = produceRandomIndex();
 
     }
+    Product.firstShown[0] = leftElementIndex;
+    Product.firstShown[1] = middleElementIndex;
+    Product.firstShown[2] = rightElementIndex;
 
     leftElement.src = Product.allProducts[leftElementIndex].source;
+    Product.allProducts[leftElementIndex].shown++;
+
     middleElement.src = Product.allProducts[middleElementIndex].source;
+    Product.allProducts[middleElementIndex].shown++;
+
     rightElement.src = Product.allProducts[rightElementIndex].source;
+    Product.allProducts[rightElementIndex].shown++;
+
+   
 
 }
 
 renderThreeImages();
 
 votedProduct.addEventListener('click', handleUserClick);
+
 function handleUserClick(event) {
     console.log(event.target.id);
     counter++;
     // counter1++;
-    if (counter <= maxAttempts ) {
+    if (counter <= maxAttempts) {
         if (event.target.id === 'left') {
             Product.allProducts[leftElementIndex].votes++;
-            Product.allProducts[leftElementIndex].shown++;
             // Product.allProducts[leftElementIndex]
         } else if (event.target.id === 'middle') {
             Product.allProducts[middleElementIndex].votes++;
-            Product.allProducts[middleElementIndex].shown++;
+
+        } else if (event.target.id === 'right') {
+            Product.allProducts[rightElementIndex].votes++;
 
         } else {
-            Product.allProducts[rightElementIndex].votes++;
-            Product.allProducts[rightElementIndex].shown++;
+            alert('please press on image');
+            counter--;
 
         }
-        console.log(Product.allProducts);
         renderThreeImages();
 
     } else {
 
         let button = document.getElementById('clickOn');
         button.addEventListener('click', showResult);
+        button.hidden = false;
+        for (let i = 0; i < Product.allProducts.length; i++) {
+            votesArr.push(Product.allProducts[i].votes);
+            shownArr.push(Product.allProducts[i].shown);
+
+        }
+        console.log(votesArr);
+
+        // show the chart
+        chart();
+
+        votedProduct.removeEventListener('click', handleUserClick)
+
+
         function showResult() {
 
             let result = document.getElementById('results-list');
             let productResult;
+
             for (let i = 0; i < Product.allProducts.length; i++) {
                 productResult = document.createElement('li');
                 result.appendChild(productResult);
-               
+
                 productResult.textContent = `${Product.allProducts[i].name} has ${Product.allProducts[i].votes}votes, and was seen ${Product.allProducts[i].shown} times `
             }
+            button.removeEventListener('click', showResult);
 
-            button.removeEventListener('click', showResult)
-            votedProduct.removeEventListener('click', handleUserClick)
         }
     }
+
+
+}
+
+function chart() {
+    let ctx = document.getElementById('myChart').getContext('2d');
+
+    let chart = new Chart(ctx, {
+        // what type is the chart
+        type: 'bar',
+
+        //  the data for showing
+        data: {
+            //  for the names
+            labels: namesArr,
+
+            datasets: [
+                {
+                    label: 'product votes',
+                    data: votesArr,
+                    backgroundColor: [
+                        'rgb(251, 93, 76)',
+                    ],
+
+                    borderWidth: 1
+                },
+
+                {
+                    label: 'product shown',
+                    data: shownArr,
+                    backgroundColor: [
+                        'black',
+                    ],
+
+                    borderWidth: 1
+                }
+
+            ]
+        },
+        options: {}
+    });
 
 }
